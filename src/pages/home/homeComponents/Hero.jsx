74d27e78738ba1a7fd2e2cab5ff6a63678391d31
kerolos <kerolosamiel4@gmail.com>
2024-07-30 17:24:23 -0700

    refactor: add animation to hero section in home page :)

--- src/pages/home/homeComponents/Hero.jsx
+++ src/pages/home/homeComponents/Hero.jsx
@@ -3,6 +3,7 @@ import Gucci from "../../../assets/hr-gucci.png";
 import Lacoste from "../../../assets/hr-lacoste.png";
 import { useEffect, useState } from "react";
 import { RiArrowLeftWideFill, RiArrowRightWideFill } from "react-icons/ri";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const heroData = [
@@ -41,14 +42,41 @@ const Hero = () => {
     <section className="hero">
       <div className="hero-container">
         <div className="slides">
-          <div className="img">
+          <motion.div
+            className="img"
+            key={`img-${index}`}
+            initial={{ opacity: 0, x: "15%", rotate: 0 }}
+            animate={{ opacity: 1, x: "-10%", rotate: -25 }}
+            transition={{ ease: "easeInOut", duration: 0.6, delay: 0.6 }}
+          >
             <img src={heroData[index].img} alt="Nike" />
-          </div>
-          <div className="title">
-            <h1>{heroData[index].title}</h1>
-            <h1>{heroData[index].title}</h1>
+          </motion.div>
 
-            <div className="circle"></div>
+          <div className="title">
+            <motion.h1
+              key={`title1-${index}`}
+              initial={{ opacity: 0, y: "-15%", x: "-50%" }}
+              animate={{ opacity: 1, y: "-50%", x: "-50%" }}
+              transition={{ ease: "easeInOut", duration: 0.6, delay: 0.2 }}
+            >
+              {heroData[index].title}
+            </motion.h1>
+            <motion.h1
+              key={`title2-${index}`}
+              initial={{ opacity: 0, y: "-15%", x: "-50%" }}
+              animate={{ opacity: 1, y: "-50%", x: "-50%" }}
+              transition={{ ease: "easeInOut", duration: 0.6, delay: 0.2 }}
+            >
+              {heroData[index].title}
+            </motion.h1>
+
+            <motion.div
+              className="circle"
+              key={`circle-${index}`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 0.5 }}
+              transition={{ ease: "easeInOut", duration: 0.3 }}
+            ></motion.div>
           </div>
 
           <button onClick={increaseIndex} className="right">
